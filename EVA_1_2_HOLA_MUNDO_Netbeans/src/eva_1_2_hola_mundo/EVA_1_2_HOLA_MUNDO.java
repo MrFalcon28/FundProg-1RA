7e980b5a14ba91517f687ac91d4eccc61496584a
MrFalcon28 <L26550240@chihuahua2.TECNM.MX>
2026-09-04 13:32:46 -0600

    Add files via upload

---
+++ EVA_1_2_HOLA_MUNDO_Netbeans/src/eva_1_2_hola_mundo/EVA_1_2_HOLA_MUNDO.java
@@ -0,0 +1,17 @@
+
+package eva_1_2_hola_mundo;
+
+public class EVA_1_2_HOLA_MUNDO {
+
+    public static void main(String[] args) { //INICIO DEL PROGRAMA
+      //A ESTO SE LE LLAMA BLOQUE DE CODIGO
+      //MOSTRAR MENSAJE DE TEXTO
+     
+      //MOSTRAR---> INSTRUCCION
+      //TEXTO ---> DATOS 
+      
+      System.out.println("Hola Mundo jeje!!");
+      
+    }//FIN DEL PROGRAMA 
+    
+}
